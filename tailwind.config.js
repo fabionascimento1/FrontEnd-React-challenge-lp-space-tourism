654ad5547ab75590278fc27848e4d483f026648d
Fabio Nascimento <agsuperpoder@gmail.com>
2022-03-18 15:02:04 -0300

    feat: add colors tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      colors: {
+        "blue-dark": '#0B0D17',
+        "blue-light": 'D0D6F9',
+        "white": '#FFF',
+        "black": '#000',
+      },
       fontFamily: {
         sans: ["Barlow", "sans-serif"],
       },
