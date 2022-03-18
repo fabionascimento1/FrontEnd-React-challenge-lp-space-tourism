6c6bc73362d5e112edf09b6afc01e77f2cd09713
Fabio Nascimento <agsuperpoder@gmail.com>
2022-03-18 16:49:39 -0300

    feat: add scroll animation

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -20,7 +20,13 @@ export default function Navbar() {
       <div className="w-full h-full flex gap-6 items-center justify-center">
         {data.map(({name}, index) => (
           <div className="text-white h-full select-none cursor-pointer hover:border-b-2 transition-all duration-300">
-            <Link className="h-full flex gap-2 items-center uppercase" to={name}>
+            <Link 
+              className="h-full flex gap-2 items-center uppercase" 
+              to={name}
+              spy={true}
+              smooth={true}
+              duration={500}  
+            >
             <p className="font-bold">0{index}</p>
             {name}
             </Link>
